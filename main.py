a363c549e897d9ab7466e17bea1d3a7a28c33199
mjadjamal12-hub <mjadjamal12@gmail.com>
2026-03-05 02:57:44 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.uix.button import Button
-class YemenTool(App):
-    def build(self):
-        # هذا هو نص الزر الذي سيظهر في تطبيقك
-        return Button(text='Yemen Unlock Tool\nVersion 1.0\nReady', font_size=30)
-if __name__ == "__main__":
-    YemenTool().run()
